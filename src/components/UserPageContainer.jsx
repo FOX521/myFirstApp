090870fb5988930d6457aea236a358f8644f70c2
Vadim <crocozybra.v@gmail.com>
2021-03-07 14:58:27 +0300

    I created class component wich was added into conect component

--- src/components/UserPageContainer.jsx
+++ src/components/UserPageContainer.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { followActionCreater, unFollowActionCreater, setUsersActionCreater, setCurrentPageActionCreater, setCountUsersActionCreater } from '../data/reducerUserPage';
-import UserPage from './UserPage'
+import axios from 'axios'
+import UsersPage from './UsersPage';
+
+class UserPageAPI extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+       axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        .then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        })
+    };
+
+    onPageChange = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        .then(response => {
+            this.props.setUsers(response.data.items);
+        })
+    }
+
+    render() {
+        return <UsersPage onPageChange={this.onPageChange} 
+                totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                currentPage ={this.props.currentPage} 
+                users={this.props.users}
+                follow={this.props.follow}
+                unFollow={this.props.unFollow} />
+    };
+};
 
 let mapStateToProps = (state) => {
     return  {
@@ -32,6 +65,6 @@ let mapDispatchToProps = (dispatch) => {
     }
 };
 
-let UserPageContainer = connect(mapStateToProps,mapDispatchToProps)(UserPage)
+let UserPageContainer = connect(mapStateToProps,mapDispatchToProps)(UserPageAPI)
 
 export default UserPageContainer;
